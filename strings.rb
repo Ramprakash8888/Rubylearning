2a29fd04fad4cd1357091c841ae5f680aeea1140
Ramprakash <84673068+Ramprakash8888@users.noreply.github.com>
2021-09-25 22:53:20 +0530

    Add files via upload

---
+++ strings.rb
@@ -0,0 +1,23 @@
+types_of_people = 10 #stores 10 in types of people
+x = "There are #{types_of_people} types of people." 
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x #prints value stored in x
+puts y #prints value stored in y
+
+puts "I said: #{x}." 
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation # types value stored in joke evaluation
+
+w = "This is the left side of..." #stores the string inside "" into w
+e = "a string with a right side." #stores the string inside "" into e
+
+puts w + e # adds both the strings store in w and e 
+
+# line 5 , line 10, line 11 are the places were string is found inside a string
