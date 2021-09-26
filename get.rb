b56c0d3e42fe7f36ab8730d951a936150d3b46db
Ramprakash <84673068+Ramprakash8888@users.noreply.github.com>
2021-09-26 15:19:29 +0530

    Add files via upload

---
+++ get.rb
@@ -0,0 +1,8 @@
+puts "How old are you? "
+age = gets
+puts "How tall are you? "
+height = gets
+puts "How much do you weigh? "
+weight = gets
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
